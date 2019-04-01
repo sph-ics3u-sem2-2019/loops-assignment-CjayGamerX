bbb165f116e105fa889c971d5ac62b7107b19c71
CJMathaniel <32065287+CjayGamerX@users.noreply.github.com>
2019-04-01 09:04:06 -0400

    Problem 2 couldnt reverse

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int input=0;
+		System.out.println("Gimmeeeeeee Numberrrr");
+		int input1 = in.nextInt();
+		while (input1>input) {
+			input=input+1;
+			System.out.println(input);
+			
+			
+		}
+	}
+		
 	}
 
-}
+
--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -5,7 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in two numbers. Add up all the values between them
 		Scanner in=new Scanner(System.in);
-
+		int input=0;
+		int total=0;
+		System.out.println("What is the starting Number?");
+		int input1 = in.nextInt();
+        System.out.println("What is the ending Number?");
+		int input2 = in.nextInt();
+		/* if (input1 >= input2) {
+			for (int input4 = input2; input4<=input1;input4--) {
+				total=total+input4; 
+			}
+			System.out.println(total); 
+		} else */
+			for (int input3 = input1;input3<=input2;input3++ ) {
+				input=input+input3;}
+			System.out.println(input);
 	}
 
 }
