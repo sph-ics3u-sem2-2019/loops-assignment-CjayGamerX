dd173bfdca55270dcfb3fb01ff94feeb75c94728
CJMathaniel <32065287+CjayGamerX@users.noreply.github.com>
2019-04-02 11:14:33 -0400

    Loops Assignment Continuation

--- src/problem3/Main.java
+++ src/problem3/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in a word and print out one character per String
 		Scanner in=new Scanner(System.in);
+		System.out.println("Give Me A DAMN Word! ");
+		String word = in.nextLine(); 
+		for (int y=0; y< word.length(); y++) {
+			System.out.println(word.charAt(y));
+			
+		}
 	}
 
 }
--- src/problem4/Main.java
+++ src/problem4/Main.java
@@ -8,7 +8,34 @@ public class Main {
 		//give them their score
 		Random rnd=new Random();
 		Scanner in=new Scanner(System.in);
+		
+		System.out.println("Its the end of the world here is a test for you!");
+		
+		int CJ = 0;
+		
+	for (int i = 0; i < 10; i++) {
+		int yo = rnd.nextInt(10);
+		int zoop = rnd.nextInt(10);
+		System.out.println(yo+"x"+zoop);
+		int Well = in.nextInt();
+		
+		if (Well!=yo*zoop) {
+			System.out.println("Your a dissapointment to your family");
+		}else {
+			System.out.println("Your not the worst person ever!!!!");
+			CJ++;
+		}
 
+	}
+	if (CJ<5) {
+		System.out.println(CJ+" /10");
+		System.out.println("You Need To Practice more");
+		if (CJ>5) {
+			System.out.println(CJ+" /10");
+			System.out.println("Keep Working Hard Dummy");
+			}
+		}
 	}
 
 }
+
