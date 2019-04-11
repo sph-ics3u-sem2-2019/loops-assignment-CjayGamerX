4d416a570cd71685e3857e7c6f2e7fd6af777085
CJMathaniel <32065287+CjayGamerX@users.noreply.github.com>
2019-04-11 09:51:45 -0400

    Update Main.java

--- src/problem5/Main.java
+++ src/problem5/Main.java
@@ -5,6 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		// Read the string and output in reverse case.
 		Scanner in=new Scanner(System.in);
-	}
-
+		
+		System.out.println("GIMME WOOORRRDDD!");
+		String word = in.nextLine();
+		
+		for (int i = 0; i < word.length(); i++) {
+			char letter=word.charAt(i);
+			boolean upper=Character.isUpperCase(letter);
+			boolean lower=Character.isLowerCase(letter);
+			if (upper==true) {
+				letter=Character.toLowerCase(letter);
+				System.out.print(letter);
+		}
+			else if (lower==true) {
+				letter=Character.toUpperCase(letter);
+				System.out.print(letter);
+			}
+			else System.out.print(letter);
+		}
+} 
+	
 }
+
+
